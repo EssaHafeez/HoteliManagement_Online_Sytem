d71818766295673f8d6e421d0bf577b6bf66e940
Essa hafeez <hafeezessa11@gmail.com>
2024-01-31 19:35:18 +0500

    Feature Search Function added

--- HotelRoom.java
+++ HotelRoom.java
@@ -2,6 +2,15 @@ public static class HotelRoom
 {
 
 
+    public static class Search_Module
+    {
+    
+        public static void Search(int date)
+        {
+                system.out.PrintIn("Date" + date);
+             
+        }
+    }
 
     
 }
